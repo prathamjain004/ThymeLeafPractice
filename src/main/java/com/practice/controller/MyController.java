589a098a8d3d6cdf7bc96e640c2163f67b7d15d8
Pratham Jain <prathamjain004@gmail.com>
2023-06-21 12:26:52 +0530

    Frontend Ui Created

--- src/main/java/com/practice/controller/MyController.java
+++ src/main/java/com/practice/controller/MyController.java
@@ -9,13 +9,12 @@ import java.util.Date;
 
 @Controller
 public class MyController {
-    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String about(Model model) {
         System.out.println("Inside about handler");
         model.addAttribute("name","Pratham Jain");
         model.addAttribute("Date",new Date().toLocaleString());
 
-        return "about";
-        // about.html
+        return "user";
     }
 }
