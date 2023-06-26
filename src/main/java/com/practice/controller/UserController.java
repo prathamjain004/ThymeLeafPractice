4d0f84a59770dabe02ac3ea88a064665cf0ab4dc
Pratham Jain <prathamjain004@gmail.com>
2023-06-26 18:21:22 +0530

    Delete Functionality Integrated

--- src/main/java/com/practice/controller/UserController.java
+++ src/main/java/com/practice/controller/UserController.java
@@ -21,17 +21,13 @@ public class UserController {
     }
 
     @GetMapping
-    public String getAllUsers(Model model, User user) {
+    public String getAllUsers(Model model) {
+         User user = new User();
          model.addAttribute("users", userService.getAllUsers());
          model.addAttribute("user", user);
          return "user";
     }
 
-//    @GetMapping("/{id}")
-//    public String getUser(@PathVariable("id") int id) {
-//        userService.getUserById(id);
-//        return "user";
-//    }
 
     @PostMapping
     public String addUser(@ModelAttribute("user") User user) {
@@ -39,16 +35,10 @@ public class UserController {
         return "redirect:/user";
     }
 
-    @DeleteMapping("/{id}")
+    @GetMapping("/{id}")
     public String deleteUser(@PathVariable("id") int id){
         userService.deleteUser(id);
         return "redirect:/user";
     }
-
-//    @DeleteMapping
-//    public String deleteAllUsers() {
-//        userService.deleteAllUsers();
-//        return "redirect:/user";
-//    }
 
 }
